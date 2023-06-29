2c606a5c98f8f6eeef8ba64a444322dadc5477f8
Robert <heinro@umich.edu>
2023-06-29 15:02:26 -0400

    Refinement of dump/load commands, add --replicate option

--- mibios/management/commands/dump_data.py
+++ mibios/management/commands/dump_data.py
@@ -1,7 +1,10 @@
+from contextlib import contextmanager
 from pathlib import Path
+import argparse
 from subprocess import Popen, PIPE
 
-from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
 from django.db import connections
 from django.db.models import ManyToManyField
 from django.db.transaction import atomic
@@ -9,8 +12,126 @@ from django.db.transaction import atomic
 from mibios import get_registry
 
 
-class Command(BaseCommand):
+COMMAND_LOCK = 'DUMP_FILE_UPLOAD_IN_PROGRESS'
+DB_SIGNAL = 'REPLICATE_THIS'
+
+MODE_ADD = 'add'
+MODE_REPLACE = 'replace'
+MODE_UPDATE = 'update'
+
+
+class DumpBaseCommand(BaseCommand):
+    """
+    Abstract dump command
+
+    Inheriting classes should implement the do_dump method.
+    """
     help = 'dump data in postgres text format'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.dumpfiles = []
+
+    def add_arguments(self, parser):
+        parser.formatter_class = argparse.ArgumentDefaultsHelpFormatter
+        parser.add_argument(
+            '--replicate',
+            action='store_true',
+            help='Get output dir via settings.REPLICATION_DUMP_PATH (unless '
+                 'overridden by -o) and save a transaction manifest file.  '
+                 'This is the counterpart to the subscription mode of the '
+                 'load_data command.',
+        )
+        parser.add_argument('-o', '--out-dir', help='output directory.')
+        parser.add_argument('--compress', action='store_true',
+                            help='compress output with zstd')
+        parser.add_argument(
+            '-m', '--mode',
+            choices=[MODE_ADD, MODE_UPDATE, MODE_REPLACE],
+            default=MODE_UPDATE,
+            help=f'Insert mode:  {MODE_ADD}: insert rows with non-existing '
+            f'IDs, raises an error if existing row ids ar encountered. '
+            f'{MODE_REPLACE}: Like "{MODE_ADD}" but truncate the table first. '
+            f' {MODE_UPDATE}: Like "{MODE_ADD}" but allow updating existing '
+            f'rows.'
+        )
+
+    def handle(self, *args, out_dir=None, compress=False, mode=None,
+               replicate=False, **options):
+        if replicate and out_dir is None:
+            out_dir = settings.REPLICATION_DUMP_PATH
+
+        if out_dir is None:
+            self.out_dir = Path()
+        else:
+            self.out_dir = Path(out_dir)
+        self.compress = compress
+        self.mode = mode
+
+        if replicate:
+            with self.replication_sender_mode():
+                self.do_dump(**options)
+        else:
+            self.do_dump(**options)
+
+    @atomic
+    def do_dump(self, **options):
+        raise NotImplementedError
+
+    @contextmanager
+    def replication_sender_mode(self):
+        command_lock = self.out_dir / COMMAND_LOCK
+        db_signal = self.out_dir / DB_SIGNAL
+
+        try:
+            command_lock.mkdir()
+        except FileExistsError as e:
+            raise CommandError(
+                f'An invocation of this command is in progress or a '
+                f'previous one did not clean up after itself: '
+                f'{e.__class__.__name__} {e}'
+            )
+
+        if db_signal.is_file():
+            command_lock.rmdir()
+            raise CommandError(
+                f'A previous dump has not been consumed yet: '
+                f'file exists: {db_signal}'
+            )
+
+        try:
+            yield
+            with db_signal.open('w') as ofile:
+                for mode, name in self.dumpfiles:
+                    ofile.write(f'{mode}\t{name}\n')
+        finally:
+            command_lock.rmdir()
+
+    def dump_from_queryset(self, cursor, queryset):
+        """ dump a single table from given queryset"""
+        if not hasattr(cursor, 'copy_expert'):
+            raise RuntimeError('requires the postgres DB backend')
+
+        query, params = queryset.query.sql_with_params()
+        sql = f'COPY ({query}) TO STDOUT HEADER'
+        sql = cursor.mogrify(sql, params)
+
+        opath = self.out_dir / queryset.model._meta.db_table
+
+        if self.compress:
+            opath = opath.with_suffix('.tab.zst')
+            zstd = ['zstd', '-T6', '-o', str(opath)]
+            with Popen(zstd, stdin=PIPE, pipesize=1024 * 1024) as p:
+                cursor.copy_expert(sql, p.stdin)
+        else:
+            opath = opath.with_suffix('.tab')
+            with opath.open('w') as ofile:
+                cursor.copy_expert(sql, ofile, 1024 * 1024)
+
+        self.dumpfiles.append((self.mode, opath.name))
+
+
+class Command(DumpBaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -19,21 +140,19 @@ class Command(BaseCommand):
             nargs='+',
             help='Name of one or more apps for which to dump data',
         )
-        parser.add_argument('-o', '--out-dir', help='output directory')
-        parser.add_argument('--compress', action='store_true',
-                            help='compress output with zstd')
+        parser.add_argument(
+            '--no-update',
+            action='store_true',
+            help='Signal to the DB to bail out with error if any row IDs '
+                 'exist already, the default is to let the DB load the data '
+                 'in UPSERT mode',
+        )
+        super().add_arguments(parser)
 
-    def handle(self, *args, appnames=None, out_dir=None, compress=False,
-               **options):
-        if out_dir is None:
-            self.out_dir = Path()
-        else:
-            self.out_dir = Path(out_dir)
-        self.compress = compress
-
-        with atomic():
-            for i in appnames:
-                self.dump_app_data(i)
+    @atomic
+    def do_dump(self, appnames=None, **options):
+        for i in appnames:
+            self.dump_app_data(i)
 
     def dump_app_data(self, appname):
         """ dump data for given app """
@@ -48,26 +167,5 @@ class Command(BaseCommand):
             for i in models + through_models:
                 self.stdout.write(f'{i._meta.model_name} ', ending='')
                 self.stdout.flush()
-                self.dump_table(cur, i)
-                self.stdout.write('[OK]\n')
-
-    def dump_table(self, cursor, model):
-        """ dump a single table """
-        query, params = model.objects.all().query.sql_with_params()
-        if params != ():
-            raise RuntimeError(
-                'unexpectedly got non-empty params with {model}: {params}'
-            )
-
-        sql = f'COPY ({query}) TO STDOUT HEADER'
-        opath = self.out_dir / model._meta.db_table
-
-        if self.compress:
-            opath = opath.with_suffix('.tab.zst')
-            zstd = ['zstd', '-T6', '-o', str(opath)]
-            with Popen(zstd, stdin=PIPE, pipesize=1024 * 1024) as p:
-                return cursor.copy_expert(sql, p.stdin)
-        else:
-            opath = opath.with_suffix('.tab')
-            with opath.open('w') as ofile:
-                return cursor.copy_expert(sql, ofile, 1024 * 1024)
+                self.dump_from_queryset(cur, i.objects.all())
+                self.stdout.write(f'{cur.rowcount} [OK]\n')
--- mibios/management/commands/load_data.py
+++ mibios/management/commands/load_data.py
@@ -2,9 +2,15 @@ from pathlib import Path
 
 from psycopg2.sql import SQL, Identifier
 
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.db import connections
-from django.db.transaction import atomic, set_rollback
+from django.db.transaction import atomic
+
+from .dump_data import MODE_ADD, MODE_REPLACE, MODE_UPDATE
+
+
+class DryRunRollback(Exception):
+    pass
 
 
 class Command(BaseCommand):
@@ -12,10 +18,12 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'infiles',
-            metavar='dumpfile',
+            'file_args',
+            metavar='infiles',
             nargs='+',
-            help='One or more input files in postgres dump text format',
+            help='One or more input files in postgres dump text format.  '
+                 'With --replicate this is the replication transaction '
+                 'listing',
         )
         parser.add_argument(
             '-n', '--dry-run',
@@ -23,47 +31,94 @@ class Command(BaseCommand):
             help='Dry run, do not actually commit anything to the database.'
         )
         parser.add_argument(
-            '-u', '--update',
+            '--replicate',
             action='store_true',
-            help='Allow updating existing rows.  By default, rows can only be '
-                 'added and trying to add an existing row raise an '
-                 'psycopg2.errors.UniqueViolation.',
+            help='Load data deposited by dump_data --replicate'
+        )
+        parser.add_argument(
+            '-m', '--mode',
+            choices=[MODE_ADD, MODE_UPDATE, MODE_REPLACE],
+            default=MODE_UPDATE,
+            help=f'Insert mode:  {MODE_ADD}: insert rows with non-existing '
+            f'IDs, raises an error if existing row ids ar encountered. '
+            f'{MODE_REPLACE}: Like "{MODE_ADD}" but truncate the table first. '
+            f' {MODE_UPDATE}: Like "{MODE_ADD}" but allow updating existing '
+            f'rows.'
         )
 
-    def handle(self, *args, infiles=None, update=False, dry_run=False,
-               **options):
-        with atomic():
-            for i in infiles:
-                self.stdout.write(f'{i} ...', ending='')
-                self.stdout.flush()
-                if update:
-                    total, changed = self.update_from_file(i)
-                    self.stdout.write(f'  {changed}/{total} [OK]\n')
-                else:
-                    rowcount = self.load_from_file(i)
-                    self.stdout.write(f'  {rowcount} [OK]\n')
-            if dry_run:
-                print('ROLLBACK')
-                set_rollback(True)
+    def handle(self, *args, file_args=None, mode=None, dry_run=False,
+               replicate=False, **options):
 
-    def load_from_file(self, path):
+        if replicate:
+            if len(file_args) > 1:
+                raise CommandError('In replication mode only a single input '
+                                   'file is accepted.')
+            job_listing = Path(file_args[0])
+            modes = []
+            data_files = []
+            with job_listing.open() as ifile:
+                for line in ifile:
+                    mode, name = line.split()
+                    modes.append(mode)
+                    infile = job_listing.parent / name
+                    if not infile.is_file():
+                        raise CommandError('no such file: {infile}')
+                    data_files.append(infile)
+        else:
+            modes = [mode] * len(file_args)
+            data_files = [Path(i) for i in file_args]
+
+        self.conn = connections['default']
+        try:
+            with atomic():
+                if MODE_REPLACE in modes:
+                    self.do_truncate(*(
+                        f for m, f
+                        in zip(modes, data_files)
+                        if m == MODE_REPLACE)
+                    )
+
+                for mode, infile in zip(modes, data_files):
+                    self.stdout.write(f'{infile} ...', ending='')
+                    self.stdout.flush()
+                    if mode == MODE_UPDATE:
+                        total, changed = self.update_from_file(infile)
+                        self.stdout.write(f'  {changed}/{total} [OK]\n')
+                    elif mode == MODE_ADD or mode == MODE_REPLACE:
+                        rowcount = self.add_from_file(infile)
+                        self.stdout.write(f'  {rowcount} [OK]\n')
+                    else:
+                        raise ValueError('bad mode')
+                if dry_run:
+                    raise DryRunRollback
+        except DryRunRollback:
+            self.stderr.write('dry run rollback')
+        else:
+            self.stdout.write('Cleanup: deleting input files... ', ending='')
+            for i in data_files:
+                i.unlink()
+            job_listing.unlink()
+            self.stdout.write('[Done]')
+
+    def add_from_file(self, path):
         """ load data into one table from given path """
         table_name, _, _ = Path(path).name.partition('.')
+
         with open(path) as ifile:
             head = ifile.readline().split()
         columns = SQL(',').join((Identifier(i) for i in head))
 
         sql = SQL('COPY {table_name} ({columns}) FROM STDIN WITH HEADER')
         sql = sql.format(table_name=Identifier(table_name), columns=columns)
-        with open(path) as ifile, connections['default'].cursor() as cur:
+        with open(path) as ifile, self.conn.cursor() as cur:
             cur.copy_expert(sql, ifile)
             return cur.rowcount
 
     def update_from_file(self, path):
         """ update a table from given path """
-        table_name, _, _ = Path(path).name.partition('.')
+        table_name, _, _ = path.name.partition('.')
         tmp_table = 'tmp_' + table_name
-        with open(path) as ifile:
+        with path.open() as ifile:
             head = ifile.readline().split()
         columns = SQL(', ').join((Identifier(i) for i in head))
         excl_cols = SQL(', ').join((
@@ -89,11 +144,28 @@ class Command(BaseCommand):
                            columns=columns, excl_cols=excl_cols)
         sql3 = sql3.format(tmp_table=tmp_table)
 
-        with open(path) as ifile, connections['default'].cursor() as cur:
+        with open(path) as ifile, self.conn.cursor() as cur:
             cur.execute(sql0)
             cur.copy_expert(sql1, ifile)
             total_rows = cur.rowcount
+
+        with self.conn.cursor() as cur:
             cur.execute(sql2)
             new_or_changed_rows = cur.rowcount
             cur.execute(sql3)
+
         return total_rows, new_or_changed_rows
+
+    def do_truncate(self, *infiles):
+        table_names = [Path(i).name.partition('.')[0] for i in infiles]
+        tables = SQL(', ').join((Identifier(i) for i in table_names))
+        sql = SQL('TRUNCATE {tables} CASCADE')
+        sql = sql.format(tables=tables)
+
+        table_names = ', '.join(table_names)
+        self.stdout.write(
+            f'Erasing all data in: {table_names} + cascade', ending=''
+        )
+        with self.conn.cursor() as cur:
+            cur.execute(sql)
+        self.stdout.write(' [OK]')
