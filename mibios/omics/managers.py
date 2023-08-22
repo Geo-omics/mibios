Geo-omics/mibios

commit 4ef110dfd7235c5895857ef3391c2df64475c8e3

omics/manager:  metagenomic data import update
  * remove the tracking_id field from Sample model
  * Sample.objects.sync() re-named to update_analysis_status()
  * For update_analysis_status() transition to re-named input file with
    somewhat different columns.  The create option of sync() is dropped.
  * Use full-length Contig.contig_id and Gene.gene_id incl. samp_NN
    prefix. Previously only the contig number relative to the sample and
    for genes, the gene number rewlative to the contig was parsed and
    used.

mibios/omics/managers.py
@@ -4,7 +4,7 @@ Module for data load managers
 === Workflow to load metagenomic data ===
 
 Assumes that UMRAD and sample meta-data is loaded
-    Sample.objects.sync()
+    Sample.objects.update_analysis_status()
     s = Sample.objects.get(sample_id='samp_14')
     Contig.loader.load_fasta_sample(s)
     Gene.loader.load_fasta_sample(s)
@@ -188,7 +188,7 @@ class AlignmentLoader(BulkLoader, SampleLoadMixin):
     load_flag_attr = 'gene_alignment_hits_loaded'
 
     def get_file(self, sample):
-        return sample.get_metagenome_path() / f'{sample.tracking_id}_GENES.m8'
+        return sample.get_metagenome_path() / f'{sample.sample_id}_GENES.m8'
 
     def query2gene_pk(self, value, obj):
         """
@@ -238,7 +238,7 @@ class CompoundAbundanceLoader(BulkLoader, SampleLoadMixin):
     def get_file(self, sample):
         return resolve_glob(
             sample.get_metagenome_path() / 'annotation',
-            f'{sample.tracking_id}_compounds_*.txt'
+            f'{sample.sample_id}_compounds_*.txt'
         )
 
 
@@ -374,9 +374,10 @@ class ContigLikeLoader(SequenceLikeLoader):
     abundance_load_flag = None  # set in inheriting class
 
     def get_contigs_file_path(self, sample):
+        # FIXME: this is unused, remove?
         return resolve_glob(
             sample.get_metagenome_path() / 'annotation',
-            f'{sample.tracking_id}_contigs_*.txt'
+            f'{sample.sample_id}_contigs_*.txt'
         )
 
     def process_coverage_header_data(self, sample, data):
@@ -494,11 +495,11 @@ class ContigLoader(ContigLikeLoader):
 
     def get_fasta_path(self, sample):
         return sample.get_metagenome_path() / 'assembly' \
-            / (sample.tracking_id + '_MCDD.fa')
+            / 'megahit_noNORM' / 'final.contigs.renamed.fa'
 
     def get_rpkm_path(self, sample):
         return sample.get_metagenome_path() / 'assembly' \
-            / f'{sample.tracking_id}_READSvsCONTIGS.rpkm'
+            / f'{sample.sample_id}_READSvsCONTIGS.rpkm'
 
     def trim_id(self, value, obj):
         """ Pre-processor to trim tracking id off, e.g. deadbeef_123 => 123 """
@@ -596,7 +597,7 @@ class FuncAbundanceLoader(BulkLoader, SampleLoadMixin):
     def get_file(self, sample):
         return resolve_glob(
             sample.get_metagenome_path() / 'annotation',
-            f'{sample.tracking_id}_functionss_*.txt'
+            f'{sample.sample_id}_functionss_*.txt'
         )
 
     spec = CSV_Spec(
@@ -618,12 +619,12 @@ class GeneLoader(ContigLikeLoader):
     def get_fasta_path(self, sample):
         return (
             sample.get_metagenome_path() / 'genes'
-            / f'{sample.tracking_id}_GENES.fna'
+            / f'{sample.sample_id}_GENES.fna'
         )
 
     def get_rpkm_path(self, sample):
         return sample.get_metagenome_path() / 'genes' \
-            / f'{sample.tracking_id}_READSvsGENES.rpkm'
+            / f'{sample.sample_id}_READSvsGENES.rpkm'
 
     def extract_gene_id(self, value, obj):
         """
@@ -743,16 +744,12 @@ class SampleManager(Manager):
     """ Manager for the Sample """
     def get_file(self):
         """ get the metagenomic pipeline import log """
-        return settings.OMICS_DATA_ROOT / 'data' / 'import_log.tsv'
+        return settings.OMICS_DATA_ROOT / 'data' / 'imported_samples.tsv'
 
     @atomic_dry
-    def sync(self, source_file=None, create=False, skip_on_error=False):
+    def update_analysis_status(self, source_file=None, skip_on_error=False):
         """
         Update sample table with analysis status
-
-        :param bool create:
-            Create a new sample if needed.  The new sample will not belong to a
-            dataset.  Not for production use.
         """
         if source_file is None:
             source_file = self.get_file()
@@ -760,18 +757,16 @@ class SampleManager(Manager):
         with open(source_file) as f:
             # check assumptions on columns
             SAMPLE_ID = 0
-            PROJECT = 1
-            TYPE = 2
-            TRACKING_ID = 7
-            ANALYSIS_DIR = 12
-            SUCCESS = 16
+            STUDY_ID = 1
+            TYPE = 3
+            ANALYSIS_DIR = 4
+            SUCCESS = 6
             cols = (
-                (SAMPLE_ID, 'Sample'),
-                (PROJECT, 'Project'),
+                (SAMPLE_ID, 'SampleID'),
+                (STUDY_ID, 'StudyID'),
                 (TYPE, 'sample_type'),
-                (TRACKING_ID, 'sampleID'),
                 (ANALYSIS_DIR, 'sample_dir'),
-                (SUCCESS, 'import_sucess'),  # [sic]
+                (SUCCESS, 'import_success'),
             )
 
             head = f.readline().rstrip('\n').split('\t')
@@ -783,57 +778,49 @@ class SampleManager(Manager):
                     )
 
             seen = []
-            track_id_seen = set()
+            samp_id_seen = set()
             unchanged = 0
+            notfound = 0
+            nosuccess = 0
             for line in f:
                 row = line.rstrip('\n').split('\t')
                 sample_id = row[SAMPLE_ID]
-                dataset = row[PROJECT]
+                dataset = row[STUDY_ID]
                 sample_type = row[TYPE]
-                tracking_id = row[TRACKING_ID]
                 analysis_dir = row[ANALYSIS_DIR]
                 success = row[SUCCESS]
 
-                if not all([sample_id, tracking_id, analysis_dir, success]):
+                if not all([sample_id, dataset, sample_type, analysis_dir, success]):  # noqa: E501
                     raise RuntimeError(f'field is empty in row: {row}')
 
-                if tracking_id in track_id_seen:
+                if sample_id in samp_id_seen:
                     # skip rev line
                     continue
                 else:
-                    track_id_seen.add(tracking_id)
+                    samp_id_seen.add(sample_id)
 
                 if success != 'TRUE':
                     log.info(f'ignoring {sample_id}: no import success')
+                    nosuccess += 1
                     continue
 
                 need_save = False
                 try:
                     obj = self.get(
                         sample_id=sample_id,
-                        # dataset__short_name=dataset,
                     )
                 except self.model.DoesNotExist:
-                    if not create:
-                        if skip_on_error:
-                            log.warning(f'no such sample: {sample_id} / '
-                                        f'{dataset} (skipping)')
-                            continue
-                        else:
-                            raise
-
-                    # create a new orphan sample
-                    obj = self.model(
-                        tracking_id=tracking_id,
-                        sample_id=sample_id,
-                        dataset=None,
-                        sample_type=sample_type,
-                        analysis_dir=analysis_dir,
-                    )
-                    need_save = True
-                    save_info = f'new sample: {obj}'
+                    # object may be hidden by default manager (private or so)
+                    log.warning(f'no such sample: {sample_id} (skipping)')
+                    notfound += 1
+                    continue
                 else:
-                    updateable = ['tracking_id', 'sample_type', 'analysis_dir']
+                    if obj.dataset.dataset_id != dataset:
+                        raise RuntimeError('{sample_id}: bad dataset')
+                    if obj.sample_type != sample_type:
+                        raise RuntimeError('{sample_id}: bad sample type')
+
+                    updateable = ['analysis_dir']
                     changed = []
                     for attr in updateable:
                         val = locals()[attr]
@@ -854,6 +841,14 @@ class SampleManager(Manager):
                     log.info(save_info)
 
                 seen.append(obj.pk)
+
+        if notfound:
+            log.warning(
+                f'Samples missing from database (or hidden): {notfound}'
+            )
+
+        if nosuccess:
+            log.warning(f'Samples not marked "import_success": {nosuccess}')
 
         if unchanged:
             log.info(f'Data for {unchanged} samples are already in the DB and '

mibios/glamr/management/commands/make_seagull_export.py
@@ -17,7 +17,6 @@ BASE_FIELDS = [
 
 EXCLUDE_FIELDS = [
     # technical field for omics app
-    'tracking_id',
     'metag_pipeline_reg',
     'analysis_dir',
     'read_count',

mibios/omics/models.py
@@ -56,12 +56,6 @@ class AbstractSample(Model):
         unique=True,
         help_text='internal sample accession',
     )
-    tracking_id = models.CharField(
-        # length of md5 checksum, 128 bit as hex string
-        max_length=32,
-        **uniq_opt,
-        help_text='internal uniform hex id',
-    )
     sample_name = models.TextField(
         max_length=32,
         **ch_opt,
@@ -91,7 +85,7 @@ class AbstractSample(Model):
     )
     metag_pipeline_reg = models.BooleanField(
         default=False,
-        help_text='is registered in metagenomic pipeline, has tracking ID',
+        help_text='is registered in metagenomic pipeline',
     )
     contig_fasta_loaded = models.BooleanField(
         default=False,
@@ -166,7 +160,7 @@ class AbstractSample(Model):
         abstract = True
 
     def __str__(self):
-        return self.sample_id or self.tracking_id or super().__str__()
+        return self.sample_id
 
     def load_bins(self):
         if not self.binning_ok:
@@ -195,10 +189,8 @@ class AbstractSample(Model):
             self.save()
 
     def get_metagenome_path(self):
-        if self.tracking_id is None:
-            raise RuntimeError(f'sample {self} has no tracking id')
         return settings.OMICS_DATA_ROOT / 'data' / 'omics' / 'metagenomes' \
-            / self.tracking_id
+            / self.sample_id
 
     def get_fq_paths(self):
         base = settings.OMICS_DATA_ROOT / 'READS'
@@ -1063,8 +1055,8 @@ class Contig(ContigLike):
 
     def set_from_fa_head(self, fasta_head_line):
 
-        # parsing ">deadbeef_123\n" -> "123"
-        self.contig_id = fasta_head_line.lstrip('>').rstrip().partition('_')[2]
+        # parsing ">samp_14_123\n" -> "samp_14_123"
+        self.contig_id = fasta_head_line.rstrip().removeprefix('>')
 
 
 class FuncAbundance(AbstractAbundance):
@@ -1139,9 +1131,8 @@ class Gene(ContigLike):
             raise ValueError('expected strand to be "1" or "-1"')
 
         # name expected to be: deadbeef_123_1
-        gene_id = gene_id.partition('_')[2]  # get e.g. 123_1
         self.gene_id = gene_id
-        contig_id = gene_id.partition('_')[0]  # get e.g. 123
+        contig_id, _, _ = gene_id.rpartition('_')  # samp_12_34_5 -> samp_12_34
         try:
             # contig_id_map must be a mapping from contig_ids to Contig PKs
             self.contig_id = contig_id_map[contig_id]
@@ -1380,17 +1371,24 @@ class Dataset(AbstractDataset):
         swappable = 'OMICS_DATASET_MODEL'
 
 
-def load_all(**kwargs):
+def load_helper_metagenome(samples=None):
     """
     Load all data
-
-    assumes an empty DB.
     """
-    verbose = kwargs.get('verbose', False)
-    get_sample_model().sync(**kwargs)
-    # ReadLibrary.sync()
-    Contig.load(verbose=verbose)
-    Bin.import_all()
-    CheckM.import_all()
-    Gene.load(verbose=verbose)
-    Protein.load(verbose=verbose)
+    if samples is None:
+        Sample = get_sample_model()
+        samples = Sample.objects.filter(
+            sample_type='metagenome',
+            metag_pipeline_reg=True,
+            gene_alignment_hits_loaded=False,
+        )
+
+    for i in samples:
+        if not Alignment.loader.get_file(i).is_file():
+            continue
+        if not i.contig_fasta_loaded:
+            Contig.loader.load_fasta_sample(i)
+        if not i.gene_fasta_loaded:
+            Gene.loader.load_fasta_sample(i)
+        Alignment.loader.load_sample(i)
+        break

commit 71e7a2c2bd2c70597a86a56a03c770b0c13b8314

omics/managers: Adjust parsing of gene/contig IDs in rpkm files
I think this was a regression from 4ef110df when contig/gene IDs started
to keep the prefix.

mibios/omics/managers.py
@@ -501,11 +501,6 @@ class ContigLoader(ContigLikeLoader):
         return sample.get_metagenome_path() / 'assembly' \
             / f'{sample.sample_id}_READSvsCONTIGS.rpkm'
 
-    def trim_id(self, value, obj):
-        """ Pre-processor to trim tracking id off, e.g. deadbeef_123 => 123 """
-        _, _, value = value.partition('_')
-        return value.upper()
-
     def calc_rpkm(self, value, obj):
         """ calculate rpkm based on total post-QC read-pairs """
         return (1_000_000_000 * int(obj.reads_mapped)
@@ -517,7 +512,7 @@ class ContigLoader(ContigLikeLoader):
                 / int(obj.length) / self.sample.read_count)
 
     rpkm_spec = BBMap_RPKM_Spec(
-        ('#Name', 'contig_id', trim_id),
+        ('#Name', 'contig_id'),
         ('Length', 'length'),
         ('Bases', 'bases'),
         ('Coverage', 'coverage'),
@@ -648,8 +643,9 @@ class GeneLoader(ContigLikeLoader):
         """
         Pre-processor to get just the gene id from prodigal fasta header
         """
-        # deadbeef_123_1 # bla bla bla => 123_1
-        return value.split(maxsplit=1)[0].partition('_')[2]
+        # deadbeef_123_1 # bla bla bla => deadbeef_123_1
+        value, _, _ = value.partition(' ')
+        return value
 
     def calc_rpkm(self, value, obj):
         """ calculate rpkm based on total post-QC read-pairs """