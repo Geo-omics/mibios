7f0a5116f92202bcbe7308cfdfd10c0e52fb8003
Robert <heinro@umich.edu>
2023-09-21 11:31:55 -0400

    omics/utils: add the Timestamper stdout redirecting utility

--- mibios/omics/tests.py
+++ mibios/omics/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from time import sleep
 
-# Create your tests here.
+from mibios.omics.utils import Timestamper
+from mibios.umrad.utils import ProgressPrinter
+
+
+def test_timestamper():
+    with Timestamper(template='[ {timestamp} testing ]  ') as obj:
+        print('starting up ...')
+        print('second line ...', end='')
+        print('still second line')
+        print(f'{vars(obj)=}')
+        print('waiting two seconds')
+        sleep(2)
+        print('multiple lines coming up', 'another line', 'third one',
+              sep='\n')
+        sleep(1)
+        print('multi\nline\nstring')
+        pp = ProgressPrinter('looping over range...')
+        for i in pp(range(1000)):
+            sleep(0.03)
+        print('loop all done, sleeping for 2s')
+        sleep(2)
+        print('ALL DONE')
--- mibios/omics/utils.py
+++ mibios/omics/utils.py
@@ -1,4 +1,7 @@
+from contextlib import redirect_stdout
+from datetime import datetime
 from operator import methodcaller
+import sys
 
 from django.apps import apps as django_apps
 from django.conf import settings
@@ -167,3 +170,129 @@ def call_each(iterable, method_name, *args, **kwargs):
     for obj in iterable:
         callmeth(obj)
         yield obj
+
+
+class Timestamper(redirect_stdout):
+    """
+    Context manager and proxy for e.g. sys.stdout to add timestamps to output
+
+    Usage:
+        with Timestamper():
+            print('starting up...')
+            do_work()  # writes to sys.stdout
+            print('all done')
+    """
+    FULL_TIMESTAMP_CUTOFF = 3599  # in secs, when to switch to full time
+    TS_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+    def __init__(self, file_obj=None, template=None, file_copy=None):
+        """
+        parameters:
+          file_obj: the file-like to replace, defaults to sys.stdout
+          template: Custom template may contain '{timestamp}' to print the time
+                and date.  Defaults to print the timestamp in square brackets.
+          file_copy:  Optional, path to hard-copy of output, to keep a
+                persistent log.
+        """
+        self._file_obj = file_obj or sys.stdout
+        self.at_new_line = True
+        self.prev_timestamp = None
+        self.indent = None
+        if template is None:
+            self.template = '[ {timestamp} ]  '
+        else:
+            self.template = template
+        self._file_copy_path = file_copy
+        self._file_copy = None
+        # set up redirect_stdout with self as the new_target:
+        super().__init__(self)
+
+    # build file copy opening into redirect_stdout context management:
+    def __enter__(self):
+        if self._file_copy_path:
+            self._file_copy = open(self._file_copy_path, 'a')
+            self._last_nl = self._file_copy.tell()
+        return super().__enter__()
+
+    def __exit__(self, exctype, excinst, exctb):
+        if self._file_copy:
+            self._file_copy.close()
+        super().__exit__(exctype, excinst, exctb)
+
+    # Implement stdout proxying:
+    def __getattr__(self, attr):
+        return getattr(self._file_obj, attr)
+
+    def _write(self, s):
+        self._file_obj.write(s)
+        if self._file_copy:
+            if s == '\r':
+                self._file_copy.seek(self._last_nl)
+                self._file_copy.truncate()
+            else:
+                self._file_copy.write(s)
+                if s.endswith('\n'):
+                    self._last_nl = self._file_copy.tell()
+
+    def write(self, s):
+        if s.startswith('\r'):
+            self._write('\r')
+            self.at_new_line = True
+            s = s.lstrip('\r')
+
+        lines = s.splitlines(keepends=True)
+        if not lines:
+            return
+
+        if self.at_new_line:
+            # start a new line with prefix
+            self.timestamp = datetime.now()
+            if self.prev_timestamp is None:
+                delta = None
+            else:
+                delta = self.timestamp - self.prev_timestamp
+                if delta.total_seconds() > self.FULL_TIMESTAMP_CUTOFF:
+                    delta = None
+
+            if delta is None:
+                ts_str = self.timestamp.strftime(self.TS_FORMAT)
+                self.full_ts_width = len(ts_str)
+            else:
+                seconds = delta.total_seconds()
+                hours = int(seconds // 3600)
+                seconds -= hours * 3600
+                minutes = int(seconds // 60)
+                seconds -= minutes * 60
+                if hours:
+                    ts_str = f'+ {hours}:{minutes:02}:{seconds:02.0f}'
+                elif minutes:
+                    ts_str = f'+ {minutes}:{seconds:02.0f}m'
+                else:
+                    ts_str = f'+ {seconds:.0f}s'
+                    if ts_str == '+ 0s':
+                        # zero seconds, leave blank
+                        ts_str = ''
+                ts_str = ts_str.rjust(self.full_ts_width)
+
+            prefix = self.template.format(timestamp=ts_str)
+            if self.indent is None:
+                self.indent = len(prefix)
+
+        else:
+            # continue or finish a previous line
+            prefix = ''
+
+        line = lines.pop(0)
+        self._write(prefix + line)
+
+        while lines:
+            line = lines.pop(0)
+            self._write(' ' * self.indent + line)
+
+        if line.endswith('\n'):
+            self.at_new_line = True
+            self.prev_timestamp = self.timestamp
+        else:
+            self.at_new_line = False
+            if self.prev_timestamp is None:
+                self.prev_timestamp = self.timestamp
