Geo-omics/mibios

commit 0cb484d84653d8f5c9674acc0c933c9cb2eb597d

ncbi_taxonomy: add ancestry m2m relation to tax nodes
The new field TaxNode.ancestors is the transitive relation from any node
along parent back to the root.  The link to root itself is implicit,
not needed anywhere, saves some space.  The TaxNode.loader gains a
method that will populate the relation.  Lineage and LCA related methods
of the TaxNode model have been worked over a lot.

mibios/ncbi_taxonomy/managers.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from pathlib import Path
 
 from django.conf import settings
@@ -7,6 +8,7 @@ from django.db.models import ForeignObjectRel
 from . import DUMP_FILES
 from mibios.umrad.utils import atomic_dry, InputFileSpec
 from mibios.umrad.manager import InputFileError, Loader as UMRADLoader
+from mibios.umrad.model_utils import delete_all_objects_quickly
 
 
 class DumpFile(InputFileSpec):
@@ -109,3 +111,82 @@ class TaxNodeLoader(Loader):
             taxid, parentid, *_ = row
             by_taxid[int(taxid)].parent = by_taxid[int(parentid)]
         self.fast_bulk_update(objs, fields=['parent'])
+        print('Populating ancestry...')
+        self.populate_ancestry()
+        print('[OK]')
+
+    def lineages(self, tree, *ancestors):
+        """
+        traverse tree left-to-right and yield full lineages
+
+        This is a helper for populating the ancestry relation
+        """
+        if tree:
+            for pk, subtree in tree.items():
+                yield from self.lineages(subtree, *ancestors, pk)
+        else:
+            yield ancestors
+
+    def ancestry_node_pairs(self, lineages):
+        """
+        Generate distinct (ancestor, descendend) pk pairs
+
+        This is a helper to populate the ancestry relation.  The lineages
+        parameter must be a iterable of all the lineages ordered left-to-right
+        (or right-to-left.)
+        """
+        # whenever some initial nodes are the same as for the previous lineage,
+        # the links between these can be skipped as we got them last time.  The
+        # index of the first fresh node is saved in the first_change_index
+        # variable
+        prev = []
+        for lin in lineages:
+            first_change_index = 0
+            for first_change_index, (a, b) in enumerate(zip(prev, lin)):
+                if a != b:
+                    break
+
+            for i in range(first_change_index, len(lin)):
+                for j in range(i):
+                    yield lin[j], lin[i]
+
+            prev = lin
+
+    @atomic_dry
+    def populate_ancestry(self):
+        """
+        Populate the ancestor m2m relation
+
+        Assumes that there is a root node with taxid 1.
+        """
+        m2m_field = self.model._meta.get_field('ancestors')
+        Through = m2m_field.remote_field.through
+        delete_all_objects_quickly(Through)
+
+        root = self.get(taxid=1)
+
+        print('  Generating tree from parent relations...', end='', flush=True)
+        # some super clever code to turn the parent relation into a tree (a
+        # dict of dicts, keys are PKs, values are sub-trees
+        dict_of_dict = lambda: defaultdict(dict)  # noqa: E731
+        forest = defaultdict(dict_of_dict)
+        for child, parent in self.values_list('pk', 'parent_id').iterator():
+            forest[parent][child] = forest[child]
+
+        # Pick the tree for root.  After this, the root, is implicit, while we
+        # keep the root node itself, we will only work with the tree below
+        # root, so the ancestor relation with it will not be saved.
+        tree = forest[root.pk]
+        del forest
+        # in NCBI taxonomy root's parent is root, that circle must be removed,
+        # as we'll do some depth-first tranversal, so we want a real tree
+        del tree[root.pk]
+        print(' [OK]')
+
+        lins = self.lineages(tree)
+        pairs = self.ancestry_node_pairs(lins)
+        through_objs = ((
+            Through(from_taxnode_id=b, to_taxnode_id=a)
+            for a, b in pairs
+        ))
+        self.bulk_create_wrapper(Through.objects.bulk_create)(through_objs)

mibios/ncbi_taxonomy/migrations/0002_auto_20230413_1611.py
@@ -1,0 +1,22 @@
+# Generated by Django 3.2.18 on 2023-04-13 20:11
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ncbi_taxonomy', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='taxname',
+            options={'verbose_name': 'taxonomic name'},
+        ),
+        migrations.AddField(
+            model_name='taxnode',
+            name='ancestors',
+            field=models.ManyToManyField(to='ncbi_taxonomy.TaxNode'),
+        ),
+    ]

mibios/ncbi_taxonomy/models.py
@@ -1,6 +1,5 @@
-from itertools import zip_longest
-
 from django.db import models
+from django.utils.functional import cached_property
 
 from mibios.umrad.model_utils import ch_opt, fk_opt, fk_req, opt
 from mibios.umrad.model_utils import Model as UmradModel
@@ -233,6 +232,7 @@ class TaxNode(Model):
     is_hgc_inherited = models.BooleanField(
         help_text='inherits hydrogenosome gencode from parent'
     )
+    ancestors = models.ManyToManyField('self', symmetrical=False)
 
     loader = TaxNodeLoader()
 
@@ -240,18 +240,31 @@ class TaxNode(Model):
         verbose_name = 'NCBI taxon'
         verbose_name_plural = 'NCBI taxa'
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # an attribute allowing alternative methods to prove the name:
+        self._name = None
+
     def __str__(self):
         return f'{self.taxid}'
 
-    @property
+    @cached_property
     def name(self):
         """
         Get the scientific name of node
 
         This works because (and as long as) each node has exactly one
-        scientific name
+        scientific name.
         """
-        return self.taxname_set.get(name_class=TaxName.NAME_CLASS_SCI)
+        if self._name is None:
+            qs = TaxName.objects.filter(
+                node=self,
+                name_class=TaxName.NAME_CLASS_SCI,
+            )
+            qs = qs.values_list('name', flat=True)
+            return qs.get()
+        else:
+            return self._name
 
     def is_root(self):
         """ Say if node is the root of the taxonomic tree """
@@ -262,59 +275,64 @@ class TaxNode(Model):
         'species',
     )
 
-    def lineage_list(self, full=True, names=True):
-        lineage = list(reversed(list(self.ancestors(all_ranks=full))))
-        if names:
-            f = dict(node_id__in=lineage, name_class=TaxName.NAME_CLASS_SCI)
-            qs = TaxName.objects.filter(**f)
-            name_map = dict(qs.values_list('node__taxid', 'name'))
-            lineage = [(i, name_map[i.taxid]) for i in lineage]
+    @cached_property
+    def lineage(self):
+        nodes = {i.pk: i for i in self.ancestors.all()}
+        lin = [self]
+        while nodes:
+            lin.insert(0, nodes.pop(lin[0].parent_id))
+        return lin
 
-        return lineage
+    def get_standard_lineage(self):
+        """ Get standard 8-rank lineage """
+        return [i for i in self.lineage if i.rank in self.STANDARD_RANKS]
 
-    def lineage(self, full=True):
-        lin = [
-            f'{i.rank}:{name}'
-            for i, name in self.lineage_list(full=full, names=True)
-        ]
+    def fetch_lineage_names(self):
+        """ fetch scientific names for all lineage nodes """
+        f = dict(
+            node__in=self.lineage,
+            name_class=TaxName.NAME_CLASS_SCI,
+        )
+        qs = TaxName.objects.filter(**f).values_list('node_id', 'name')
+        names = dict(qs)
+        for i in self.lineage:
+            i._name = names[i.pk]
+
+    def lineage_fmt(self, full=False):
+        """ Return formatted lineage """
+        if full:
+            lin = self.lineage
+        else:
+            lin = self.get_standard_lineage()
+        lin = [f'{i.rank}:{i.name}' for i in lin]
         return ';'.join(lin)
 
-    def ancestors(self, all_ranks=True):
+    def get_lca_lineage(self, *others, full=False):
         """
-        Generate a node's ancestor nodes starting with itself, towards the root
+        Calculate the LCA between this and another TaxNode
 
-        Will always yield self even if all_ranks is False.
+        Returns the corresponding lineage as list of TaxNodes.
+        Returns an empty list if LCA is root.
         """
-        cur = self
-        while True:
-            if all_ranks or cur.rank in self.STANDARD_RANKS or cur is self:
-                yield cur
-            if cur.is_root():
-                break
-            cur = cur.parent
-
-    def lca(self, other, all_ranks=True):
-        seen_a = set()
-        seen_b = set()
-        a_anc = self.ancestors(all_ranks=all_ranks)
-        b_anc = other.ancestors(all_ranks=all_ranks)
-        for a, b in zip_longest(a_anc, b_anc):
-            if a is not None:
-                seen_a.add(a)
-            if b is not None:
-                seen_b.add(b)
-
-            if a in seen_b:
-                return a
-            if b in seen_a:
-                return b
-
-        # all_ranks == False but lca is above superkingdom
-        return None
+        lca = []
+        lineages = [i.lineage for i in [self] + list(others)]
+        for our_node, *other_nodes in zip(*lineages):
+            for i in other_nodes:
+                if i != our_node:
+                    break
+            else:
+                # all nodes the same
+                if full or our_node.rank in self.STANDARD_RANKS:
+                    lca.append(our_node)
+        return lca
 
     def __eq__(self, other):
-        # It's taxid or nothing
-        return self.taxid == other.taxid
+        try:
+            # It's taxid or nothing
+            return self.taxid == other.taxid
+        except AttributeError:
+            # other has no taxid attr
+            return False
 
     def is_ancestor_of(self, other):
         """
@@ -322,11 +340,7 @@ class TaxNode(Model):
 
         This method is the base of the rich comparison method
         """
-        # TODO: optimize by checking on rank?
-        for i in other.ancestors(all_ranks=True):
-            if self == i:
-                return True
-        return False
+        return other in self.lineage
 
     def __lt__(self, other):
         return self.is_ancestor_of(other) and not self == other

commit ec76677a1a6ae01f610051c70fe55f9a9b2cf931

taxonomy: fix crash loading data
A regression from 0cb484d8.  The TaxNode.ancestors field is not loaded
from a dump file.  This patch adds the Loader.spec_skip_fields class
attribute a mechanism to exclude fields from the input file spec.

mibios/ncbi_taxonomy/managers.py
@@ -31,6 +31,8 @@ class DumpFile(InputFileSpec):
             # get all fields defined by our model in order
             fields = loader.model.get_fields(skip_auto=True, with_m2m=True).fields  # noqa: E501
             for i in fields:
+                if i.name in loader.spec_skip_fields:
+                    continue
                 if isinstance(i, ForeignObjectRel):
                     # filter out reverse fields
                     continue
@@ -62,6 +64,10 @@ class DumpFile(InputFileSpec):
 
 class Loader(UMRADLoader):
     default_load_kwargs = dict(bulk=True, update=True)
+
+    spec_skip_fields = []
+    """ Fields that don't get loaded via dump file, the spec setup will ignore
+    them """
 
     def setup_spec(self, spec=None, **kwargs):
         if spec is None and self.spec is None:
@@ -96,6 +102,8 @@ class CitationLoader(Loader):
 
 
 class TaxNodeLoader(Loader):
+    spec_skip_fields = ['ancestors']
+
     @atomic_dry
     def load(self, **kwargs):
         super().load(**kwargs)