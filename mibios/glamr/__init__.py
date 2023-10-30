59c37599ee448830f45c9525da2d3a2f31ac5598
Robert <heinro@umich.edu>
2023-10-30 13:42:42 -0400

    glamr: add HOWTO populate the database

--- mibios/glamr/__init__.py
+++ mibios/glamr/__init__.py
@@ -1,4 +1,27 @@
-""" sub-package for the GLAMR DB and website """
+"""
+sub-package for the GLAMR DB and website
+
+glamr depends on omics, ncbi_taxonomy, and umrad apps.
+
+Populating the database:
+
+    from mibios.omics.models import *
+    from mibios.glamr.models import *
+    Sample.loader.load_all_meta_data()
+
+    from mibios.ncbi_taxonomy import load()
+    load()
+
+    from mibios.umrad.models import *
+    CompoundRecord.loader.load(skip_on_error=True)
+    ReactionRecord.loader.load(skip_on_error=True)
+    UniRef100.loader.load(skip_on_error=True)
+    FuncRefDBEntry.name_loader.load()
+
+    Sample.loader.load_metagenomic_data()
+
+
+"""
 
 GREAT_LAKES = [
     'Lake Erie',
