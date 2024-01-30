ad1f2186c2c810917c8c35dafa82f1c79255906b
Robert <heinro@umich.edu>
2024-01-30 10:38:01 -0500

    glamr/admin: make the fake user actually work
    And changed the class name to make it clear that no authentication takes
    place.

--- mibios/glamr/admin.py
+++ mibios/glamr/admin.py
@@ -13,22 +13,25 @@ class FakeUser(User):
         return True
 
 
-class AdminSite(admin.AdminSite):
+class OpenAdminSite(admin.AdminSite):
     site_header = 'GLAMR site administration'
 
     def has_permission(self, request):
         try:
-            request.user = User.objects.filter(is_staff=True).first()
+            # try for a staff user, who must be in the DB
+            user = User.objects.filter(is_staff=True).first()
         except User.DoesNotExist:
             # the fake user is good to look at the admin site but when trying
             # to save changes will fail on the log entry creation as that needs
             # a user in the database.
-            request.user = FakeUser()
+            user = None
+
+        request.user = user or FakeUser()
         return True
 
 
 if settings.INTERNAL_DEPLOYMENT and settings.ENABLE_OPEN_ADMIN:
-    admin_site = AdminSite(name='glamr_admin')
+    admin_site = OpenAdminSite(name='glamr_admin')
     admin_site.register(AboutInfo)
     admin_site.register(Credit)
 else:
