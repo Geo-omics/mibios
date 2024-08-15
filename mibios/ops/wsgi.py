29433bbea6738f1a20c2fa888c3538926dac7b9c
Robert <heinro@umich.edu>
2024-08-15 15:19:23 -0400

    wsgi: print notice with timestamp and version info to stdout

--- mibios/ops/wsgi.py
+++ mibios/ops/wsgi.py
@@ -6,11 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
-
+from datetime import datetime
 import os
 from pathlib import Path
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+from mibios import __version__ as version
 
 
 DEFAULT_SETTINGS = 'mibios.ops.settings'
@@ -23,3 +26,5 @@ else:
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
 application = get_wsgi_application()
+print(f'[{datetime.now()}] wsgi settings:{settings} version:{version}',
+      file=sys.stderr)
