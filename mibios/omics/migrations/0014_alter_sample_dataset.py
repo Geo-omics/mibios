13ad0555028ca5631af15b019824774574385cbb
Robert <heinro@umich.edu>
2025-04-18 10:57:37 -0400

    omics/migrations: fix not using swappable dataset model
    Unclear how this made it into the migration.  This removes a dependency
    of omics to the glamr app.

--- mibios/omics/migrations/0014_alter_sample_dataset.py
+++ mibios/omics/migrations/0014_alter_sample_dataset.py
@@ -16,6 +16,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='sample',
             name='dataset',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='glamr.dataset'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.OMICS_DATASET_MODEL),
         ),
     ]
