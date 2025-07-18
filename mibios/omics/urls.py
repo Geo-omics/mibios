f271f66e8a6c32945794a4fcc4b92ebb59a8abf9
Robert <heinro@umich.edu>
2025-07-18 16:25:38 -0400

    views: add page for a contig's sequence
      * give contigs their own detail view, lives in glamr app, previously
        used the generic RecordView
      * moves "extra urls on detail page" code from SampleView to new
        ContigView, previously SampleView was the only user of that facility
      * new sequence view, lives in omics app
      * uses simple <code> tag for sequence display, a bit primitive and
        ugly

--- mibios/omics/urls.py
+++ mibios/omics/urls.py
@@ -9,4 +9,5 @@ from . import views
 urlpatterns = [
     path('sample-tracker/', views.SampleTrackingView.as_view(), name='sample_tracking'),  # noqa:E501
     path('download-files/', views.FileListingView.as_view(), name='file_listing'),  # noqa:E501
+    path('contig/<int:pk>/sequence/', views.ContigSequenceView.as_view(), name='contig_seq'),  # noqa:E501
 ]
