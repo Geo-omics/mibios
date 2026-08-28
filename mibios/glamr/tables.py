Geo-omics/mibios

commit 3dc75fcb058434652f9a7504933b2c379f332f03

glamr/html: improvements to table rendering
  * add ordering arrows to table headers
    - implemented by modifying django-tables2's bootstrap5.html template
    - name on non-sorted columns acts like descending
    - name on sorted columns turns off sorting, i.e. ?sort=
      (it's impossible with the django-tables2 machinery to remove the
      sort= altogether)
    - the two arrows are inside a css styled span to prevent
      linebreaking between the arrows
  * reduce white space (w/bootstrap5's table-sm)

mibios/glamr/tables.py
@@ -21,6 +21,11 @@ class Table(Table0):
     tables have all (modulo exclusions + internal) fields.  Do not use this
     together with Meta.fields. """
 
+    class Meta:
+        attrs = {
+            'class': 'table table-sm',
+        }
+
     def __init__(self, data=None, view=None, exclude=None, **kwargs):
         self.view = view
 
@@ -29,6 +34,7 @@ class Table(Table0):
             f'no {self._meta.model._meta.verbose_name} records for given '
             f'parameters'
         )
+        kwargs.setdefault('template_name', 'glamr/table_bootstrap5.html')
 
         exclude = list(exclude) if exclude else []
 
@@ -220,7 +226,7 @@ def linkify_value(value):
 
 
 class AboutHistoryTable(Table):
-    class Meta:
+    class Meta(Table.Meta):
         # NOTE on order: Most recent version should go on top of page.  But
         # can't order by PK as id column is excluded.  So as unpublished is
         # NULL it'll go first with Postgres (good for production) but last with
@@ -235,7 +241,7 @@ class CompoundAbundanceTable(Table):
     sample = Column(linkify=linkify_value)
     compound = Column(linkify=linkify_value)
 
-    class Meta:
+    class Meta(Table.Meta):
         model = omics_models.CompoundAbundance
         exclude = ['id']
 
@@ -244,7 +250,7 @@ class ContigTable(Table):
     sample = Column(linkify=linkify_value)
     name = Column(linkify=linkify_record, empty_values=())
 
-    class Meta:
+    class Meta(Table.Meta):
         model = omics_models.Contig
         sequence = ['sample', 'name', '...']
 
@@ -263,7 +269,7 @@ class DBInfoTable(Table):
 
     GB = 1024 * 1024 * 1024
 
-    class Meta:
+    class Meta(Table.Meta):
         # Using model pg_class here, but the model dbstat is similar enough, so
         # this table class should work for postgresql as well as sqlite.  When
         # display with sqlite, django_tables2 will show a warning about the
@@ -308,7 +314,7 @@ class FileTable(OmicsFileTable):
         empty_values=(),  # trigger render_FOO()
     )
 
-    class Meta:
+    class Meta(Table.Meta):
         model = omics_models.File
         fields = ['file', 'filetype', 'size', 'modtime']
         sequence = ['file', 'filetype', 'size', 'modtime']
@@ -335,7 +341,7 @@ class FunctionAbundanceTable(Table):
     sample = Column()
     sum_tpm = Column()
 
-    class Meta:
+    class Meta(Table.Meta):
         model = omics_models.FuncAbundance
         exclude = ['id']
 
@@ -344,7 +350,7 @@ class FunctionNameAbundanceTable(Table):
     sample = Column()
     sum_tpm = Column()
 
-    class Meta:
+    class Meta(Table.Meta):
         model = omics_models.FunctionNameAbundance
         exclude = ['id']
 
@@ -353,7 +359,7 @@ class ReadAbundanceTable(Table):
     sample = Column(linkify=linkify_value)
     ref = Column(linkify=linkify_value, verbose_name='Function/reference')
 
-    class Meta:
+    class Meta(Table.Meta):
         model = omics_models.ReadAbundance
         exclude = ['id']
 
@@ -404,7 +410,7 @@ class ReadAbundanceTable(Table):
 
 
 class ReferenceTable(Table):
-    class Meta:
+    class Meta(Table.Meta):
         model = glamr_models.Reference
         exclude = ('abstract',)
         order_by = '-year'
@@ -414,7 +420,7 @@ class TaxNodeTable(Table):
     taxid = Column(linkify=linkify_record)
     parent = Column(linkify=linkify_value)
 
-    class Meta:
+    class Meta(Table.Meta):
         model = TaxNode
         fields = ('taxid', 'rank', 'name', 'parent')
 
@@ -433,7 +439,7 @@ class TaxonAbundanceTable(Table):
     rank = Column(accessor='taxon__rank')
     tax_name = Column(accessor='taxon__name', verbose_name='Tax Name')
 
-    class Meta:
+    class Meta(Table.Meta):
         model = omics_models.TaxonAbundance
         fields = ['sample', 'taxon', 'rank', 'tax_name', 'tpm']
         order_by = ['-tpm']
@@ -483,7 +489,7 @@ class DatasetAccessTable(Table0):
     restricted_to = ManyToManyColumn(verbose_name='allowed groups')
     access = Column('group id access list')
 
-    class Meta:
+    class Meta(Table.Meta):
         model = glamr_models.Dataset
         fields = ['dataset_id', 'scheme', 'sample_count']
         sequence = ['dataset_id', 'primary_ref', 'scheme', 'sample_count',
@@ -563,7 +569,7 @@ class DatasetTable(Table):
     html_fields = ['scheme', 'samples', 'primary_ref', 'water_bodies',
                    'material_type', 'sample_type', 'external_urls']
 
-    class Meta:
+    class Meta(Table.Meta):
         model = glamr_models.Dataset
         sequence = ['scheme', 'samples', 'primary_ref', 'water_bodies', '...']
         empty_text = 'No dataset / study information available'
@@ -671,7 +677,7 @@ class SampleTable(Table):
         'dataset',
     )
 
-    class Meta:
+    class Meta(Table.Meta):
         model = glamr_models.Sample
         sequence = ['sample_name', 'sample_type', '...', 'dataset']
         empty_text = 'no samples found'
@@ -723,7 +729,7 @@ class AssayTable(Table):
         'sample_name', 'accession', 'sample_type', 'amplicon_target', 'primers',
     )
 
-    class Meta:
+    class Meta(Table.Meta):
         model = omics_models.SeqSample
         sequence = [
             'sample_name', 'sample_type', 'sra_accession', 'amplicon_target', 'primers',

mibios/glamr/static/glamr/css/base.css
@@ -113,3 +113,5 @@
 .metatranscriptome-legend {
 	color: var(--col-metatranscriptome);
 }
+
+table thead tr th svg.bi-arrow-up { margin-left: -5px; }  /* less space between the arrows *?

mibios/glamr/templates/glamr/table_bootstrap5.html
@@ -1,0 +1,160 @@
+{% comment "copyright notice" %}
+This file is a modified bootstrap5.html from django-tables2 v2.7.5
+
+Copyright: 2008, Michael Elsdörfer <http://elsdoerfer.name>
+ Jan Pieter Waagmeester <jieter@zostera.nl>
+ Bradley Ayers <brad@dovetailapp.com>
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ 1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above
+    copyright notice, this list of conditions and the following
+    disclaimer in the documentation and/or other materials
+    provided with the distribution.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+{% endcomment %}
+
+{% load django_tables2 %}
+{% load i18n l10n %}
+{% block table-wrapper %}
+<div class="table-container">
+    {% block table %}
+        <table {% render_attrs table.attrs class="table" %}>
+            {% block table.thead %}
+            {% if table.show_header %}
+                <thead {{ table.attrs.thead.as_html }}>
+                <tr>
+                {% for column in table.columns %}
+                <th {{ column.attrs.th.as_html }} scope="col">{%spaceless%}
+                        {% if column.orderable %}
+                            {%if column.is_ordered%}
+                                <a class="link-secondary link-underline-primary link-opacity-25-hover" href="{% querystring table.prefixed_order_by_field='' %}">
+                                {{ column.header }}
+                                </a>
+                                <span style="white-space:nowrap;">
+                                {%if column.order_by_alias.is_descending%}
+                                {%include "arrow-down.svg"%}
+                                <a class="link-opacity-25-hover" href="{% querystring table.prefixed_order_by_field=column.order_by_alias.opposite %}">
+                                    {%include "arrow-up.svg"%}
+                                </a>
+                                {%else%}
+                                <a class="link-opacity-25-hover" href="{% querystring table.prefixed_order_by_field=column.order_by_alias.opposite %}">
+                                    {%include "arrow-down.svg"%}
+                                </a>
+                                {%include "arrow-up.svg"%}
+                                </span>
+                                {%endif%}
+                            {%else%}
+                            <a class="link-opacity-25-hover" href="{% querystring table.prefixed_order_by_field=column.order_by_alias.opposite %}">
+                                {{ column.header }}
+                            </a>
+                            <span style="white-space:nowrap;">
+                            <a class="link-opacity-25-hover" href="{% querystring table.prefixed_order_by_field=column.order_by_alias.opposite %}">
+                                {%include "arrow-down.svg"%}
+                            </a>
+                            <a class="link-opacity-25-hover" href="{% querystring table.prefixed_order_by_field=column.order_by_alias %}">
+                                {%include "arrow-up.svg"%}
+                            </a>
+                            </span>
+                            {%endif%}
+                        {% else %}
+                            {{ column.header }}
+                        {% endif %}
+                        {%endspaceless%}
+                    </th>
+                {% endfor %}
+                </tr>
+                </thead>
+            {% endif %}
+            {% endblock table.thead %}
+            {% block table.tbody %}
+                <tbody {{ table.attrs.tbody.as_html }}>
+                {% for row in table.paginated_rows %}
+                    {% block table.tbody.row %}
+                    <tr {{ row.attrs.as_html }}>
+                        {% for column, cell in row.items %}
+                            <td {{ column.attrs.td.as_html }}>{% if column.localize == None %}{{ cell }}{% else %}{% if column.localize %}{{ cell|localize }}{% else %}{{ cell|unlocalize }}{% endif %}{% endif %}</td>
+                        {% endfor %}
+                    </tr>
+                    {% endblock table.tbody.row %}
+                {% empty %}
+                    {% if table.empty_text %}
+                    {% block table.tbody.empty_text %}
+                        <tr><td colspan="{{ table.columns|length }}">{{ table.empty_text }}</td></tr>
+                    {% endblock table.tbody.empty_text %}
+                    {% endif %}
+                {% endfor %}
+                </tbody>
+            {% endblock table.tbody %}
+            {% block table.tfoot %}
+            {% if table.has_footer %}
+            <tfoot {{ table.attrs.tfoot.as_html }}>
+                <tr>
+                {% for column in table.columns %}
+                    <td {{ column.attrs.tf.as_html }}>{{ column.footer }}</td>
+                {% endfor %}
+                </tr>
+            </tfoot>
+            {% endif %}
+            {% endblock table.tfoot %}
+        </table>
+    {% endblock table %}
+
+    {% block pagination %}
+        {% if table.page and table.paginator.num_pages > 1 %}
+        <nav aria-label="Table navigation">
+            <ul class="pagination justify-content-center">
+            {% if table.page.has_previous %}
+                {% block pagination.previous %}
+                <li class="previous page-item">
+                    <a href="{% querystring table.prefixed_page_field=table.page.previous_page_number %}" class="page-link">
+                        <span aria-hidden="true">&laquo;</span>
+                        {% trans 'previous' %}
+                    </a>
+                </li>
+                {% endblock pagination.previous %}
+            {% endif %}
+            {% if table.page.has_previous or table.page.has_next %}
+            {% block pagination.range %}
+            {% for p in table.page|table_page_range:table.paginator %}
+                <li class="page-item{% if table.page.number == p %} active{% endif %}">
+                    <a class="page-link" {% if p != '...' %}href="{% querystring table.prefixed_page_field=p %}"{% endif %}>
+                        {{ p }}
+                    </a>
+                </li>
+            {% endfor %}
+            {% endblock pagination.range %}
+            {% endif %}
+            {% if table.page.has_next %}
+                {% block pagination.next %}
+                <li class="next page-item">
+                    <a href="{% querystring table.prefixed_page_field=table.page.next_page_number %}" class="page-link">
+                        {% trans 'next' %}
+                        <span aria-hidden="true">&raquo;</span>
+                    </a>
+                </li>
+                {% endblock pagination.next %}
+            {% endif %}
+            </ul>
+        </nav>
+        {% endif %}
+    {% endblock pagination %}
+</div>
+{% endblock table-wrapper %}

commit 1130d0aabd354a459b3e50bd8709e26d9b333044

glamr/tables: fix wrong dataset table template, regression from 3dc75fc
Just put the template name into Meta and let inheritance do the work.

mibios/glamr/tables.py
@@ -65,6 +65,7 @@ class Table(Table0):
         attrs = {
             'class': 'table table-sm',
         }
+        template_name = 'glamr/table_bootstrap5.html'
 
     def __init__(self, data=None, view=None, exclude=None, **kwargs):
         self.view = view
@@ -74,7 +75,6 @@ class Table(Table0):
             f'no {self._meta.model._meta.verbose_name} records for given '
             f'parameters'
         )
-        kwargs.setdefault('template_name', 'glamr/table_bootstrap5.html')
 
         exclude = list(exclude) if exclude else []
 