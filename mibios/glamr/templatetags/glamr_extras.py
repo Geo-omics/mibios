0f56436218e986cdfc657e448c86de9aec4d2cfe
Robert <heinro@umich.edu>
2026-08-31 16:19:36 -0400

    glamr/html: implement redirect links in login / logout workflow
      * menu's login link gets current URL encoded in querystring
      * menu's logout link gets current URL if it can be accessed without
        login, else the default redirect is in effect
      * the StaffLoginRequiredMixin gets simplified and now inherits from
        LoginRequiredMixin, this makes a test nav_linker simpler; the
        cache_control->private stuff is removed, this seems to work
        correctly without

--- mibios/glamr/templatetags/glamr_extras.py
+++ mibios/glamr/templatetags/glamr_extras.py
@@ -1,5 +1,8 @@
 from django import template
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Field, Q
+from django.http.request import QueryDict
 from django.template.defaultfilters import capfirst, stringfilter
 from django.urls import resolve, reverse
 from django.urls.exceptions import Resolver404
@@ -104,7 +107,8 @@ def record_url(*args, **kwargs):
 
 
 @register.simple_tag(takes_context=True)
-def nav_linker(context, url_name, url_text, html_class='', dropdown=False):
+def nav_linker(context, url_name, url_text, html_class='', dropdown=False,
+               next_url_query=False):
     """
     Helper for nav bar links
 
@@ -127,10 +131,27 @@ def nav_linker(context, url_name, url_text, html_class='', dropdown=False):
         # rendering the Server Error 500 response (no request in context?!)
         pass
 
+    if next_url_query and not isinstance(context.get('view'), LoginRequiredMixin):
+        # Append a ?next=CURRENT_URL querystring in support of login/logout workflow
+        # If the current page is restricted to staff, then we're presumably
+        # doing the logout link and we'll do the default redirect (to the
+        # frontpage or whatever)
+        query = QueryDict(mutable=True)
+        path = context['request'].path
+        if inner_query := context['request'].GET:
+            query[REDIRECT_FIELD_NAME] = f'{path}?{inner_query.urlencode()}'
+        else:
+            query[REDIRECT_FIELD_NAME] = path
+
+        query = f'?{query.urlencode()}'
+    else:
+        query = ''
+
     return format_html(
-        '<a class="{}" href="{}">{}</a>',
+        '<a class="{}" href="{}{}">{}</a>',
         html_class,
         reverse(url_name),
+        query,
         url_text,
     )
 
